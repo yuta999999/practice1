0c469a86a87d88b4d36d2bf908b85846b66ce2b6
yuta999999 <44456785+yuta999999@users.noreply.github.com>
2024-12-01 22:35:30 +0900

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const $p = document.getElementById("text");
-const $button = document.getElementById("changeTextButton");
+const $1button = document.getElementById("changeTextButton");
+const $2button = document.getElementById("colorButton");
 
-
-$button.addEventListener("click", function(){
+$1button.addEventListener("click", function(){
   $p.innerText = "テキストが変更されました！";
 })
 
-
+$2button.addEventListener("click",function(){
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  const $body = document.querySelector("body");
+  console.log($body);
+  console.log(randomColor);
+  $body.style.backgroundColor = randomColor;
+})
